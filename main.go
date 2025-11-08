bde4d6331b7312d385bc0abce6647ee06225233f
mudimurtala <ibnmuhyideen95@gmail.com>
2025-11-08 15:30:18 +0100

    Initial commit for the quad project

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "quad/piscine"
+
+func main() {
+	piscine.QuadD(5, 3)
+}
---
+++ piscine/quadc.go
@@ -0,0 +1,28 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func QuadC(x, y int) {
+	if x <= 0 || y <= 0 {
+		return
+	}
+
+	for row := 1; row <= y; row++ {
+		for col := 1; col <= x; col++ {
+			if row == 1 && col == 1 {
+				z01.PrintRune('A')
+			} else if row == 1 && col == x {
+				z01.PrintRune('A')
+			} else if row == y && col == 1 {
+				z01.PrintRune('C')
+			} else if row == y && col == x {
+				z01.PrintRune('C')
+			} else if (row == 1) || (row == y) || (col == 1) || (col == x) {
+				z01.PrintRune('B')
+			} else {
+				z01.PrintRune(' ')
+			}
+		}
+		z01.PrintRune('\n')
+	}
+}
